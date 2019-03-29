af20a48193e8e0d55b8b97f23927733e180819ef
paul <per245@radadmins-MacBook-Pro.local>
2019-03-29 07:25:19 -0500

    modified welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @heading = "Welcome World"
-    @body = "this is the beginnings of a great relationship"
+    @body = "this is the beginnings of a beautiful relationship"
   end
 end
