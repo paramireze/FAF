63948e4e7d0914861e91d07606f38899b8162697
Paul <paramireze@gmail.com>
2019-03-31 11:59:06 -0500

    set validation for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  validates :name, :presence => true
+  validates :top_category, :presence => true
+  validates :order, :presence => true
+  validates :order, numericality: { only_integer: true }
 end
