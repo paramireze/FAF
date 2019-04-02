26ddccb8e145e03918283967beb893d0cef46794
Paul <paramireze@gmail.com>
2019-04-02 07:08:20 -0500

    add tests for question model

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @valid_question = questions(:valid_question)
+    @nil_text = questions(:nil_text)
+  end
+
+  test 'valid question' do
+    assert @valid_question.valid?
+  end
+
+  test 'question is invalid because of missing text' do
+    refute @nil_text.valid?, 'question is valid without having text'
+    assert_not_nil @nil_text.errors[:text], 'no validation error for text present'
+  end
+
+  test 'question belongs to a category named Abstracts' do
+    assert @valid_question.category.name == "Abstracts"
+  end
+
+  test 'question is not required' do
+    assert @valid_question.required
+  end
+
 end
