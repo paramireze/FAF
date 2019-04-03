f7beb44dc6ece1d765263611e64f54169a9cf6f4
Paul <paramireze@gmail.com>
2019-04-02 20:01:40 -0500

    delete initial model created for tutorial

--- app/models/joke.rb
+++
@@ -1,2 +0,0 @@
-class Joke < ApplicationRecord
-end
--- db/migrate/20190327234937_create_jokes.rb
+++
@@ -1,9 +0,0 @@
-class CreateJokes < ActiveRecord::Migration[5.1]
-  def change
-    create_table :jokes do |t|
-      t.text :body
-
-      t.timestamps
-    end
-  end
-end
