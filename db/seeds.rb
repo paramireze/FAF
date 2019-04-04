fc529bf4382e19787c7d12adce590cff7408eac0
Paul <paramireze@gmail.com>
2019-04-03 20:29:08 -0500

    seed user paul

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 =begin
-Category:
+
+"Category"
   name:text
   description:text
   top_category:text
@@ -20,17 +16,17 @@ Category:
     "created_at", null: false
     "updated_at", null: false
     t.index ["category_id"], name: "index_questions_on_category_id"
-  end
-=end
 
+"users"
+  t.text :first_name
+  t.text :last_name
+  t.boolean :active
+  t.text :email
+  t.text :user_name
+
+=end
 
 category_abstracts = Category.create([name:'Abstracts', top_category:'Research', sub_category:nil, order:1])
 category_published_peer_review = Category.create([name:'Published Peer-Reviewed Manuscripts  ', top_category:'Research', sub_category:nil, order:2])
-
-question_abstract_text = Question.create([
-    category:category_abstracts.first,
-    text:'abstracts text',
-    helper_text:'Include Authors, Presentation Title, Presentation Date, City, State or Country',
-    required:true,
-    order:1,
-    question_type:'text area'])
+question_abstract_text = Question.create([category:category_abstracts.first, text:'abstracts text', helper_text:'Include Authors, Presentation Title, Presentation Date, City, State or Country', required:true, order:1, question_type:'text area'])
+user_paul = User.create([first_name:'Paul', last_name:'Ramirez', active:true, email:'pramirez@uwhealth.org', user_name:'per245'])
